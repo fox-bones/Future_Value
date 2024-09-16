db8ec3927a7dce054e2e610c9d392d5927e00b91
fox-bones <mrcaysizzle@gmail.com>
2024-09-16 12:30:42 -0400

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using FutureValue.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FutureValue.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.FV = 0;
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(FutureValueModels model)
+        {
+            if (ModelState.IsValid) 
+            {
+                ViewBag.Fv = model.CalculateFutureValue();
+            }
+            else  
+            { 
+                ViewBag.Fv = 0; 
+            }
+
+            return View(model);
+        }
+    }
+}
